c4391641428b4e9c6085201252099d86e8bc66ca
Roberto Padilla <robert.ipo@hotmail.com>
2019-12-30 15:43:37 -0600

    implementing

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -1,8 +1,16 @@
-const Appointment = require('../models/appointment').Appointment;
+const Appointment = require('../models').Appointment;
+const Physician = require('../models').Physician;
+const Patient = require('../models').Patient;
 
 module.exports = {
     async all(request, response) {
+        const appointments = await Appointment.findAll({
+            include: [Patient, Physician]
+        });
+
         response.status(200)
-            .send({message: 'Testing'});
+            .send({
+                appointments: appointments
+            });
     }
 }
